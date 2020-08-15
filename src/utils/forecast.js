f204a097301463ab2d665587eb57d05299abe357
Anisha <anishatiwari695@gmail.com>
2020-08-15 16:43:55 +0530

    forecast updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,16 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url ='http://api.weatherstack.com/current?access_key=cea2847ffb85532caefdb4d1dac1e075&query=' + latitude + ',' + longitude +'&units=f'
+  const url ="http://api.weatherstack.com/current?access_key=c2b515d704a9c87e59badbd5640d0347&query=" +latitude +longitude +"&units=f";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(undefined,body.current.weather_descriptions +" It is currently " +body.current.temperature +" degress out. There is a " +body.current.precip +"% chance of rain. The humidity rate is " +body.current.humidity +"% out there");
+    }
+  });
+};
 
-    request({ url, json: true }, (error, {body}) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-         callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip+ '% chance of rain.')
-            // callback(undefined,  ' It is currently '+ response.body.current.weather_descriptions+ ' but actualyy it is '+response.body.current.precip)
-        }
-    })
-}
-
-module.exports = forecast
+module.exports = forecast;
